fc942a99fe005631bb084e1037d6996b21a929fe
vinualex21 <alexvinu2001@gmail.com>
2022-05-19 22:05:30 +0100

    Student implemented

---
+++ StudentEncapsulation/StudentEncapsulation/Program.cs
@@ -0,0 +1,8 @@
+﻿using StudentEncapsulation;
+
+Student student1 = new("Vinu", Grade.C, Group.Group_3);
+student1.Upgrade();
+student1.Upgrade();
+student1.Upgrade();
+student1.Upgrade();
+Console.WriteLine($"Student {student1.Name} has grade {student1.Grade} and belongs to group {student1.Group}");
---
+++ StudentEncapsulation/StudentEncapsulation/Student.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentEncapsulation
+{
+    public class Student
+    {
+        public string Name { get; private set; }
+        public Grade Grade { get; private set; }
+
+        private Group _group;
+        public int Group { 
+            get 
+            { 
+                return (int)this._group;
+            }
+            private set 
+            { 
+                
+            } 
+        }
+        private string _secretNickName;
+
+        public Student(string name, Grade grade, Group group)
+        {
+            Name = name;
+            Grade = grade;
+            _group = group;
+            _secretNickName = "MySecretNickName";
+        }
+
+        public void Upgrade()
+        {
+            if(Grade != Grade.A)
+                Grade--;
+        }
+
+        public void Downgrade()
+        {
+            if(Grade != Grade.F)
+                Grade++;
+        }
+
+    }
+
+    public enum Grade
+    {
+        A,
+        B,
+        C,
+        D,
+        E,
+        F
+    };
+
+    public enum Group
+    {
+        Group_1 = 1,
+        Group_2 = 2,
+        Group_3 = 3,
+        Group_4 = 4,
+        Group_5 = 5
+    }
+}
